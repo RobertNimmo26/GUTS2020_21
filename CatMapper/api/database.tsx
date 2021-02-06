cef136e2b89585e5c6895356ce531f47e52b8539
Archie <a.j.o.gregory@gmail.com>
2021-02-06 18:13:55 +0000

    DB now returns cats

--- CatMapper/api/database.tsx
+++ CatMapper/api/database.tsx
@@ -3,35 +3,60 @@ import "firebase/firestore";
 import { db } from "../config/keys";
 
 export const addCoords = (
-  name: any,
-  latitude: any,
-  longitude: any,
-  description: any
+    name: any,
+    latitude: any,
+    longitude: any,
+    description: any
 ) => {
-  db.ref("/cats").push({
-    name: name,
-    latitude: latitude,
-    longitude: longitude,
-    description: description,
-    createdAt: firebase.default.firestore.FieldValue.serverTimestamp(),
-  });
+    db.ref("/cats").push({
+        name: name,
+        latitude: latitude,
+        longitude: longitude,
+        description: description,
+        createdAt: firebase.default.firestore.FieldValue.serverTimestamp(),
+    });
 };
 
 export const getCoords = () => {
-  const cats: { [index: string]: never[] } = [];
-  const ref = db.ref("/cats");
-  ref.once("value", (snapshot) => {
-    snapshot.forEach((snapshotchild) => {
-      snapshotchild.forEach((data) => {
-        const obj = {};
-        Object.keys(cats).forEach((item) => {
-          obj[cats[item].name] = {
-            latitude: cats[item].latitude,
-            longitude: cats[item].longitude,
-          };
-        });
-        console.log(obj);
-      });
+    
+    const ref = db.ref("/cats"); 
+    ref.on("value", function(snapshot: any[]){
+        // Array of Cats
+        var cats: any[] = [];
+        // Array of Coordinates
+        var coords = [];
+        snapshot.forEach((cat) => {
+            // Pushing the Cat object one by one to the array
+            cats.push(cat.val());
+            // Pushing the Latitude and Longitude objects pair by pair to the array
+            coords.push([cat.val().latitude, cat.val().longitude]);
+            // console.log(typeof (cat.val().latitude));
+            // console.log((cat.val().latitude));
+        })
+        return cats;
+
+        
+
+    }, function (errorObject: { code: string; }) {
+        console.log("The read failed: " + errorObject.code);
     });
-  });
+
+
+
+    // const cats: { [index: string]: never[] } = [];
+    // const ref = db.ref("/cats");
+    // ref.once("value", (snapshot) => {
+    //   snapshot.forEach((snapshotchild) => {
+    //     snapshotchild.forEach((data) => {
+    //       const obj = {};
+    //       Object.keys(cats).forEach((item) => {
+    //         obj[cats[item].name] = {
+    //           latitude: cats[item].latitude,
+    //           longitude: cats[item].longitude,
+    //         };
+    //       });
+    //       console.log(obj);
+    //     });
+    //   });
+    // });
 };
