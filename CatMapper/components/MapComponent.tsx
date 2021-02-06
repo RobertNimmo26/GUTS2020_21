6a55956c08a5ccc12d83dc9cc2010661e90a7fa2
Robert Nimmo <2379875n@student.gla.ac.uk>
2021-02-06 19:18:18 +0000

    fix map and added heatmap

--- CatMapper/components/MapComponent.tsx
+++ CatMapper/components/MapComponent.tsx
@@ -1,24 +1,109 @@
 import React, { useState } from "react";
-import MapView from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
-import ENV from "../config/env";
+import { Heatmap } from "react-native-maps";
 
-export default function MapComponent(props: { latitude: number; longitude: number }) {
-  let latitude = props.latitude;
-  let longitude = props.longitude;
+//import fetchLocation from "../screens/TabOneScreen";
 
+export default function MapComponent(props: {
+  latitude: number;
+  longitude: number;
+}) {
+  const latitude = props.latitude;
+  const longitude = props.longitude;
 
+  console.log(latitude, longitude);
 
-
+  let points = [
+    { latitude: 6.83646681, longitude: 79.77121907, weight: 1 },
+    { latitude: 6.82776681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.82176681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83176681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83976681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.83076681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.82776681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.82076681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.82076681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.81076681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.83276681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.81976681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.867319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.865319, weight: 1 },
+    { latitude: 6.83646681, longitude: 79.77121907, weight: 1 },
+    { latitude: 6.82776681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.82176681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83176681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83976681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.83076681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.82776681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.82076681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.82076681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.81076681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.83276681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.81976681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.867319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.865319, weight: 1 },
+    { latitude: 6.84076681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83646681, longitude: 79.77121907, weight: 1 },
+    { latitude: 6.82776681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.82176681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83176681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83976681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.83076681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.82776681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.82076681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.82076681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.81076681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.83276681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.81976681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.867319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.865319, weight: 1 },
+    { latitude: 6.84076681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.841776681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.83646681, longitude: 79.77121907, weight: 1 },
+    { latitude: 6.82776681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.82176681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83176681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.83976681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.83076681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.82776681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.82076681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.82076681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.81076681, longitude: 79.861319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.83276681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.81976681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.867319, weight: 1 },
+    { latitude: 6.83776681, longitude: 79.865319, weight: 1 },
+    { latitude: 6.84076681, longitude: 79.871319, weight: 1 },
+    { latitude: 6.841776681, longitude: 79.869319, weight: 1 },
+    { latitude: 6.84076681, longitude: 79.871319, weight: 1 },
+  ];
   return (
     <View style={styles.container}>
-      <MapView 
-      initialRegion={{
-        latitude: latitude,
-        longitude: longitude
-          }}
-      style={styles.map} 
-      showsUserLocation={true} />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.0121,
+        }}
+      >
+        <Heatmap
+          points={[{ latitude: latitude, longitude: longitude, weight: 1 }]}
+          opacity={1}
+          radius={40}
+        />
+      </MapView>
     </View>
   );
 }
--- CatMapper/screens/TabOneScreen.tsx
+++ CatMapper/screens/TabOneScreen.tsx
@@ -11,84 +11,83 @@ import { addCoords, getCoords } from "../api/database";
 import { useEffect, useState } from "react";
 
 export default function TabOneScreen() {
-    const [location, setLocation] = useState(Object);
-    const [errorMsg, setErrorMsg] = useState("");
-    const [latitude, setLatitude] = useState(0);
-    const [longitude, setLongitude] = useState(0);
+  const [location, setLocation] = useState(Object);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
 
-    useEffect(() => {
-        (async () => {
-            fetchLocation();
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      fetchLocation();
+    })();
+  }, []);
 
-    let text = <Text>Waiting...</Text>;
-    if (errorMsg != "") {
-        text = <Text>Error</Text>;
-    } else if (location) {
-        text = (
-            <Text>
-                {latitude} {longitude}
-            </Text>
-        );
-    }
-
-    return (
-        <View style={styles.container}>
-            <MapComponent latitude={latitude} longitude={longitude} />
-            
-            <View
-                style={styles.separator}
-                lightColor="#eee"
-                darkColor="rgba(255,255,255,0.1)"
-            />
-            <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-            <Button
-                title={"Press me"}
-                color="#841584"
-                onPress={() => { fetchLocation()
-                                 addCoords("hello", { latitude }, { longitude }, "test")}}
-            />
-            <Button title={"Press me2"} color="#841584" onPress={() => getCoords()} />
-            <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-            <Text style={styles.title}>
-                Coords are: {text}
-                {"\n"}
-            </Text>
-        </View>
+  let text = <Text>Waiting...</Text>;
+  if (errorMsg != "") {
+    text = <Text>Error</Text>;
+  } else if (location) {
+    text = (
+      <Text>
+        {latitude} {longitude}
+      </Text>
     );
+  }
 
-    async function fetchLocation() {
-        let { status } = await Location.requestPermissionsAsync();
-        if (status !== "granted") {
-            setErrorMsg("Permission to access location was denied");
-            return;
-        }
+  return (
+    <View style={styles.container}>
+      <MapComponent latitude={latitude} longitude={longitude} />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Button
+        title={"Press me"}
+        color="#841584"
+        onPress={() => {
+          fetchLocation();
+          addCoords("hello", { latitude }, { longitude }, "test");
+        }}
+      />
+      <Button title={"Press me2"} color="#841584" onPress={() => getCoords()} />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>
+        Coords are: {text}
+        {"\n"}
+      </Text>
+    </View>
+  );
 
-        let location = await Location.getCurrentPositionAsync({});
-        setLocation(location);
-        setLatitude(location.coords.latitude);
-        setLongitude(location.coords.longitude);
-
-        return true;
+  async function fetchLocation() {
+    let { status } = await Location.requestPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
     }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+    setLatitude(location.coords.latitude);
+    setLongitude(location.coords.longitude);
+
+    return true;
+  }
 }
 
-
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: "bold",
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: "80%",
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
 });
